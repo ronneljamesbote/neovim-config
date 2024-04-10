563c14905e27cae55340c9ed96ffc3bd4af6140e
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2024-04-10 03:44:53 +0300

    Add custom vim options

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,32 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.blade.php",
+  group = augroup("BladeFiletypeRelated", { clear = true }),
+  callback = function()
+    vim.opt.filetype = "blade"
+  end,
+})
+
+autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.templ",
+  group = augroup("TemplFiletypeRelated", { clear = true }),
+  callback = function()
+    vim.opt.filetype = "templ"
+  end,
+})
+
+vim.opt.colorcolumn = "120"
+
+vim.opt.foldmethod = "manual"
+
+vim.opt.foldenable = false
+
+vim.opt.number = true
+
+vim.opt.relativenumber = true
