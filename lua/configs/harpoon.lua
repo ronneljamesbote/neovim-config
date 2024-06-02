7af997c185e7b9cc53d77dc5433af99751191a3a
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2024-06-02 19:04:45 +0300

    Add ability to delete entry in harpoon telescope list

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -4,20 +4,42 @@ return {
     harpoon:setup {}
 
     local conf = require("telescope.config").values
+
     local function toggle_telescope(harpoon_files)
       local file_paths = {}
+
       for _, item in ipairs(harpoon_files.items) do
         table.insert(file_paths, item.value)
+      end
+
+      local make_finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+        return require("telescope.finders").new_table {
+          results = paths,
+        }
       end
 
       require("telescope.pickers")
           .new({}, {
             prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table {
-              results = file_paths,
-            },
+            finder = make_finder(),
             previewer = conf.file_previewer {},
             sorter = conf.generic_sorter {},
+            attach_mappings = function(prompt_buffer_number, map)
+              map("i", "<C-d>", function()
+                local state = require "telescope.actions.state"
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                harpoon:list():remove_at(selected_entry.index)
+                current_picker:refresh(make_finder())
+              end)
+
+              return true
+            end,
           })
           :find()
     end
