a7b2ae4a79b4fbe263812030b20b410e6461c13f
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2024-06-22 15:29:46 +0300

    Fix harpoon telescope toggle command

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -3,20 +3,14 @@ return {
     local harpoon = require "harpoon"
     harpoon:setup {}
 
-    local conf = require("telescope.config").values
-
     local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      local make_finder = function()
+      local finder = function()
         local paths = {}
+
         for _, item in ipairs(harpoon_files.items) do
           table.insert(paths, item.value)
         end
+
         return require("telescope.finders").new_table {
           results = paths,
         }
@@ -25,19 +19,24 @@ return {
       require("telescope.pickers")
           .new({}, {
             prompt_title = "Harpoon",
-            finder = make_finder(),
-            previewer = conf.file_previewer {},
-            sorter = conf.generic_sorter {},
-            attach_mappings = function(prompt_buffer_number, map)
+            finder = finder(),
+            previewer = false,
+            sorter = require("telescope.config").values.generic_sorter {},
+            layout_config = {
+              height = 0.4,
+              width = 0.5,
+              prompt_position = "top",
+              preview_cutoff = 120,
+            },
+            attach_mappings = function(prompt_bufnr, map)
               map("i", "<C-d>", function()
                 local state = require "telescope.actions.state"
                 local selected_entry = state.get_selected_entry()
-                local current_picker = state.get_current_picker(prompt_buffer_number)
+                local current_picker = state.get_current_picker(prompt_bufnr)
 
-                harpoon:list():remove_at(selected_entry.index)
-                current_picker:refresh(make_finder())
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
               end)
-
               return true
             end,
           })
