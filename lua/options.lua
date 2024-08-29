e1f48d46cade9b7e4a8bb5d1394284009d8a7ac9
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2024-08-30 00:33:14 +0800

    Add restore cursor position script

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,7 @@
 require "nvchad.options"
 require "helpers.load_filetypes_associations"
 require "helpers.always_show_one_tab"
+require "helpers.restore_cursor_position"
 
 vim.opt.colorcolumn = "120"
 
