adaa654b6a6438cdbf38c99f3176a0cd26ff37ea
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2026-06-04 02:13:39 +0800

    Update nvim-tree config

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,5 +1,32 @@
 return {
   opts = {
+    view = {
+      float = {
+        enable = true,
+        quit_on_focus_loss = true,
+        open_win_config = function()
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+          -- Make the window full screen
+          local window_w = screen_w
+          local window_h = screen_h
+
+          -- Center the window
+          local center_x = (screen_w - window_w) / 2
+          local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+
+          return {
+            border = "rounded",
+            relative = "editor",
+            row = center_y,
+            col = center_x,
+            width = window_w,
+            height = window_h,
+          }
+        end,
+      },
+    },
     git = {
       enable = true,
     },
@@ -18,6 +45,7 @@ return {
     filesystem_watchers = {
       enable = true,
       ignore_dirs = {
+        "vendor",
         "node_modules",
         "packages/web/.next",
       },
