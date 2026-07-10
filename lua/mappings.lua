a72d331f2776a0f685eddadb5da3eb545cda9404
ronneljamesbote <ronneljames.miranda.bote@gmail.com>
2026-07-10 14:44:29 +0800

    Fix harpoon keymap not working

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,45 @@
 require "nvchad.mappings"
+local harpoon = require "harpoon"
+
+local function toggle_harpoon_telescope(harpoon_files)
+  local finder = function()
+    local paths = {}
+
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return require("telescope.finders").new_table {
+      results = paths,
+    }
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = finder(),
+      previewer = false,
+      sorter = require("telescope.config").values.generic_sorter {},
+      layout_config = {
+        height = 0.4,
+        width = 0.5,
+        prompt_position = "top",
+        preview_cutoff = 120,
+      },
+      attach_mappings = function(prompt_bufnr, map)
+        map("i", "<C-d>", function()
+          local state = require "telescope.actions.state"
+          local selected_entry = state.get_selected_entry()
+          local current_picker = state.get_current_picker(prompt_bufnr)
+
+          table.remove(harpoon_files.items, selected_entry.index)
+          current_picker:refresh(finder())
+        end)
+        return true
+      end,
+    })
+    :find()
+end
 
 local map = vim.keymap.set
 local nomap = vim.keymap.del
@@ -46,3 +87,32 @@ map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "TmuxNavigate move to w
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "TmuxNavigate move to window right" })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "TmuxNavigate move to window down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "TmuxNavigate move to window up" })
+
+-- Harpoon keymaps
+map("n", "<C-e>", function()
+  toggle_harpoon_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
+map("n", "<leader>a", function()
+  harpoon:list():add()
+end, { desc = "Add harpoon entry" })
+
+map("n", "<C-y>", function()
+  harpoon:list():select(1)
+end)
+map("n", "<C-u>", function()
+  harpoon:list():select(2)
+end)
+map("n", "<C-i>", function()
+  harpoon:list():select(3)
+end)
+map("n", "<C-o>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+map("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+map("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
